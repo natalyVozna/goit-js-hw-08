991e644e72bde1720a7c22153e7a08e0343c7d3e
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-07 13:16:40 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from './lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form');
 const emailEl = document.querySelector('input[name="email"]');
