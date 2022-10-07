a3c4542850baae38d8f5fb5f9a288a6d7d9214a0
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-07 10:44:36 +0300

    update package json

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const form = document.querySelector('form');
+console.log('farm', form);
 
+function handleChangeForm(e) {
+  e.preventDefault();
+  console.log(e);
+
+  const {} = e.elements;
+}
+
+form.addEventListener('input', handleChangeForm);
