083809378e58353f229781e0129395373287d4f0
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-07 12:59:56 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,58 @@
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('form');
-console.log('farm', form);
+const emailEl = document.querySelector('input[name="email"]');
+const messageEl = document.querySelector('textarea[name="message"]');
+const dataLocalStorage = localStorage.getItem('feedback-form-state');
 
-function handleChangeForm(e) {
-  e.preventDefault();
-  console.log(e);
+console.log('reload');
 
-  const {} = e.elements;
+let dataParse;
+
+if (dataLocalStorage) {
+  try {
+    dataParse = JSON.parse(dataLocalStorage);
+    emailEl.value = dataParse.email || '';
+    messageEl.value = dataParse.message || '';
+  } catch (error) {
+    console.log('error', error);
+  }
+
+  console.log('dataParse', dataParse);
 }
 
-form.addEventListener('input', handleChangeForm);
+const dataObj = {};
+function handleChangeForm(e) {
+  e.preventDefault();
+  console.log(e.target.name, e.target.value);
+  dataObj[e.target.name] = e.target.value;
+  //   let dataPars = '';
+
+  if (Object.keys(dataObj).length > 0) {
+    let data;
+    try {
+      data = JSON.stringify(dataObj);
+    } catch (error) {
+      console.log(error, 'error');
+    }
+
+    console.log('data.strin', data, dataObj);
+    localStorage.setItem('feedback-form-state', data);
+  }
+}
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  const { email, message } = e.target.elements;
+
+  console.log({
+    [email.name]: email.value,
+    [message.name]: message.value,
+  });
+
+  localStorage.setItem('feedback-form-state', '');
+  e.currentTarget.reset();
+}
+
+form.addEventListener('input', throttle(handleChangeForm, 500));
+form.addEventListener('submit', handleSubmitForm);
